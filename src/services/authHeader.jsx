02fed01b6fa351b8b89944e7f8c206526b5bf704
TiredShaman <arielangelogadoelca@gmail.com>
2025-05-03 17:30:17 +0800

    mao nani

--- src/services/authHeader.jsx
+++ src/services/authHeader.jsx
@@ -3,11 +3,14 @@ export default function authHeader() {
 
   const headers = {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'https://assessmatefinal-6cog.vercel.app',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept, Authorization',
+    'Access-Control-Allow-Credentials': 'true'
   };
 
   if (user?.token) {
     headers.Authorization = `Bearer ${user.token}`;
-    // Add additional security headers
     headers['X-Requested-With'] = 'XMLHttpRequest';
     headers['Cache-Control'] = 'no-cache';
   }
